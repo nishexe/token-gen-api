53280e900bce5a9ca9951ee00fbad61d601d0ce1
Manish Bag <manishbag007@gmail.com>
2025-05-24 15:04:59 +0530

    generate jwt token

--- src/index.js
+++ src/index.js
@@ -1,5 +1,40 @@
+import "dotenv/config";
 import express from "express";
 import crypto from "crypto";
-app = express();
+const app = express();
 const PORT = 5959;
-app.use()
+
+const JWTHeader = {
+  alg: "HS256",
+  typ: "JWT",
+};
+
+const JWTPayload = {
+  sub: "090999",
+  name: "einzeL",
+  admin: true,
+};
+
+const base64urlEncode = (obj) =>
+  Buffer.from(JSON.stringify(obj))
+    .toString("base64")
+    .replace(/=/g, "")
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_");
+
+const headerEncoded = base64urlEncode(JWTHeader);
+const payloadEncoded = base64urlEncode(JWTPayload);
+
+const secret = process.env.SECRET_KEY;
+
+const signatureEncoded = crypto
+  .createHmac("sha256", secret)
+  .update(`${headerEncoded}.${payloadEncoded}`)
+  .digest("base64")
+  .replace(/=/g, "")
+  .replace(/\+/g, "-")
+  .replace(/\//g, "_");
+
+const JWT = `${headerEncoded}.${payloadEncoded}.${signatureEncoded}`;
+console.log(JWT);
+console.log("\n");
